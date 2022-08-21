a748c04cf798655aeec450b014ad7ec6cf8ccf9a
Marios <30930688+mariospapaz@users.noreply.github.com>
2022-08-21 15:02:33 +0300

    added formatters for python and lua

--- lua/pkgs/null-ls.lua
+++ lua/pkgs/null-ls.lua
@@ -1,6 +1,6 @@
-local null_ls_status_ok, null_ls = pcall(require, 'null-ls')
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-    return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,41 +11,42 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-    debug = false,
-    sources = {
-        formatting.prettier.with({ extra_args = { '--no-semi', '--single-quote', '--jsx-single-quote' } }),
-        formatting.black.with({
-            args = { '--stdin-filename', '$FILENAME', '--quiet', '-', '--fast', '--line-length', '79' },
-        }),
-        formatting.stylua,
-        formatting.shfmt,
-        formatting.sqlformat.with({
-            extra_args = {
-                '--keywords',
-                'upper',
-                '--identifiers',
-                'upper',
-                '--indent_width',
-                '2',
-                '--indent_after_first',
-                -- '--reindent',
-                -- '--indent_columns',
-                -- '--reindent_aligned',
-                '--use_space_around_operators',
-            },
-        }),
-        -- formatting.sqlfluff,
-        -- formatting.sqlfluff.with({
-        --   args = { 'fix', '--disable_progress_bar', '-f', '-n', '-' },
-        -- }),
-        -- formatting.clang_format.with({ args = { '-style="{BasedOnStyle: llvm, IndentWidth: 4}"' } }),
-        diagnostics.flake8,
-        diagnostics.shellcheck,
-        diagnostics.zsh,
-        -- diagnostics.sqlfluff,
-        -- diagnostics.sqlfluff.with({
-        --   args = { 'lint', '-f', 'github-annotation', '-n', '--disable_progress_bar', '-' },
-        -- }),
-        code_actions.shellcheck,
-    },
+	debug = false,
+	sources = {
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.black.with({
+			args = { "--stdin-filename", "$FILENAME", "--quiet", "-", "--fast", "--line-length", "79" },
+		}),
+
+		formatting.stylua,
+		formatting.shfmt,
+		formatting.sqlformat.with({
+			extra_args = {
+				"--keywords",
+				"upper",
+				"--identifiers",
+				"upper",
+				"--indent_width",
+				"2",
+				"--indent_after_first",
+				-- '--reindent',
+				-- '--indent_columns',
+				--   '--reindent_aligned',
+				"--use_space_around_operators",
+			},
+		}),
+		-- formatting.sqlfluff,
+		-- formatting.sqlfluff.with({
+		--   args = { 'fix', '--disable_progress_bar', '-f', '-n', '-' },
+		-- }),
+		-- formatting.clang_format.with({ args = { '-style="{BasedOnStyle: llvm, IndentWidth: 4}"' } }),
+		diagnostics.flake8,
+		diagnostics.shellcheck,
+		diagnostics.zsh,
+		-- diagnostics.sqlfluff,
+		-- diagnostics.sqlfluff.with({
+		--   args = { 'lint', '-f', 'github-annotation', '-n', '--disable_progress_bar', '-' },
+		-- }),
+		code_actions.shellcheck,
+	},
 })
