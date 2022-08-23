9587d619e88b9fde924e2f37178607895241eb17
Marios <30930688+mariospapaz@users.noreply.github.com>
2022-08-23 22:14:24 +0300

    removed boilerplate

--- init.lua
+++ init.lua
@@ -6,8 +6,23 @@
     |__/|__/\___/_/\___/\____/_/ /_/ /_/\___/   \__/\____/  /_/ /_/ /_/\__, /  /_/ /_/\___/\____/|___/_/_/ /_/ /_/      
                                                                     /____/                                            
 
-]]--
+]]
+--
 
 require("options")
 require("keybinds")
-require("plugins") 
+require("plugins")
+require("./pkgs/lsp-installer")
+require("./pkgs/tree-sitter")
+require("./pkgs/telescope")
+require("./pkgs/nvim-tree")
+require("./pkgs/cmp")
+require("./pkgs/presence")
+require("./pkgs/lsp-config")
+require("./pkgs/alpha")
+require("./pkgs/project")
+require("./pkgs/which-key")
+require("./pkgs/toggleterm")
+require("./pkgs/renamer")
+require("./pkgs/bufferline")
+require("./pkgs/null-ls")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,135 +3,132 @@ local fn = vim.fn
 local command = vim.api.nvim_command
 
 -- Automatically install packer
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system({
-        'git',
-        'clone',
-        '--depth',
-        '1',
-        'https://github.com/wbthomason/packer.nvim',
-        install_path,
-    })
-    print('Installing packer close and reopen Neovim...')
-    command('packadd packer.nvim')
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	command("packadd packer.nvim")
 end
 
-local status_ok, packer = pcall(require, 'packer')
+local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-    return
+	return
 end
 
 packer.startup(function()
-    use 'wbthomason/packer.nvim'
-    
-    use 'folke/tokyonight.nvim'
-    
-    use 'nvim-treesitter/nvim-treesitter'
-    
-    use 'williamboman/nvim-lsp-installer'
+	use("wbthomason/packer.nvim")
 
-    use 'akinsho/bufferline.nvim'   
-    
-    use 'jose-elias-alvarez/null-ls.nvim'
-    
-    use 'hrsh7th/cmp-nvim-lsp'
-    
-    use 'hrsh7th/cmp-buffer'
-    
-    use 'hrsh7th/cmp-path'
-    
-    use 'ahmedkhalf/project.nvim' 
+	use("folke/tokyonight.nvim")
 
-    use 'hrsh7th/cmp-cmdline'
-    
-    use 'hrsh7th/nvim-cmp'
-    
-    use 'goolord/alpha-nvim'
-    
-    use 'saadparwaiz1/cmp_luasnip'
-    
-    use 'L3MON4D3/LuaSnip'
+	use("nvim-treesitter/nvim-treesitter")
 
-    use 'andweeb/presence.nvim'
+	use("williamboman/nvim-lsp-installer")
 
-    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+	use("akinsho/bufferline.nvim")
 
-    use {"akinsho/toggleterm.nvim", tag = 'v2.*'}
+	use("jose-elias-alvarez/null-ls.nvim")
 
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim'} }
-    }
+	use("hrsh7th/cmp-nvim-lsp")
 
-    use {
-      'filipdutescu/renamer.nvim',
-      branch = 'master',
-      requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    
-    use {
-      'kyazdani42/nvim-tree.lua',
-      requires = {
-        'kyazdani42/nvim-web-devicons', -- optional, for file icons
-      }
-    }
+	use("hrsh7th/cmp-buffer")
 
-    use {
-    'abecodes/tabout.nvim',
-      config = function()
-      require('tabout').setup {
-      tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
-      backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
-      act_as_tab = true, -- shift content if tab out is not possible
-      act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-      default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-      default_shift_tab = '<C-d>', -- reverse shift default action,
-      enable_backwards = true, -- well ...
-      completion = true, -- if the tabkey is used in a completion pum
-      tabouts = {
-        {open = "'", close = "'"},
-        {open = '"', close = '"'},
-        {open = '`', close = '`'},
-        {open = '(', close = ')'},
-        {open = '[', close = ']'},
-        {open = '{', close = '}'}
-      },
-      ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-      exclude = {} -- tabout will ignore these filetypes
-  }
-    end,
-	  wants = {'nvim-treesitter'}, -- or require if not used so far
-	  after = {'nvim-cmp'} -- if a completion plugin is using tabs load it before
-  }
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if PACKER_BOOTSTRAP then
-        require('packer').sync()
-    end
+	use("hrsh7th/cmp-path")
 
-    use 'folke/which-key.nvim'
+	use("ahmedkhalf/project.nvim")
 
+	use("hrsh7th/cmp-cmdline")
+
+	use("hrsh7th/nvim-cmp")
+
+	use("goolord/alpha-nvim")
+
+	use("saadparwaiz1/cmp_luasnip")
+
+	use("L3MON4D3/LuaSnip")
+
+	use("andweeb/presence.nvim")
+
+	use("neovim/nvim-lspconfig") -- Configurations for Nvim LSP
+
+	use({ "akinsho/toggleterm.nvim", tag = "v2.*" })
+
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+
+	use({
+		"filipdutescu/renamer.nvim",
+		branch = "master",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icons
+		},
+	})
+
+	use({
+		"abecodes/tabout.nvim",
+		config = function()
+			require("tabout").setup({
+				tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
+				backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
+				act_as_tab = true, -- shift content if tab out is not possible
+				act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+				default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+				default_shift_tab = "<C-d>", -- reverse shift default action,
+				enable_backwards = true, -- well ...
+				completion = true, -- if the tabkey is used in a completion pum
+				tabouts = {
+					{ open = "'", close = "'" },
+					{ open = '"', close = '"' },
+					{ open = "`", close = "`" },
+					{ open = "(", close = ")" },
+					{ open = "[", close = "]" },
+					{ open = "{", close = "}" },
+				},
+				ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+				exclude = {}, -- tabout will ignore these filetypes
+			})
+		end,
+		wants = { "nvim-treesitter" }, -- or require if not used so far
+		after = { "nvim-cmp" }, -- if a completion plugin is using tabs load it before
+	})
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		packer.sync()
+	end
+
+	use("folke/which-key.nvim")
 end)
 
-local status_ok, lualine = pcall(require, 'lualine')
+local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-    return
-end 
-
-if status_ok then 
-  vim.cmd("colorscheme tokyonight")
+	return
 end
 
-lualine.setup {
-  options = {
-    theme = 'tokyonight'
-  }
-} 
+if status_ok then
+	vim.cmd("colorscheme tokyonight")
+end
 
-require('./packages')
+lualine.setup({
+	options = {
+		theme = "tokyonight",
+	},
+})
